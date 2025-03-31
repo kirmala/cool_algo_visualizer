7d7723a3974ee037832623efe8499b3ccd993812
Dmitry Titov <dimatitov1428@mail.ru>
2025-03-31 19:37:02 +0700

    продолжение для сергея

--- hotweb.js
+++ hotweb.js
@@ -4,3 +4,23 @@ let inputContainer = document.getElementById("starInput");
 starBtn.addEventListener("click", function () {
     inputContainer.classList.toggle("hidden");
 });
+
+function table(){
+    let size = document.getElementById("sizeTable").value;
+    let cont = document.getElementById("tab");
+
+    if (!size) alert("Введите число дебилы!!");
+
+    cont.innerHTML = "";
+    cont.style.gridTemplateColumns = `repeat(${size}, 40px)`;
+    cont.style.gridTemplateRows = `repeat(${size}, 40px)`;
+
+
+
+    for(let i = 0; i < size * size; i++){
+        let addDiv = document.createElement("div");
+        addDiv.classList.add("btnTab");
+
+    }
+
+}
